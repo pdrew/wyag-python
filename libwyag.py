253f92c56f7dfe7624000a2b468dc313a2eaa98d
pdrew <pdrew@tessituranetwork.com>
2024-04-03 21:07:29 +1100

    Add object read, write helper functions, blob class

--- libwyag.py
+++ libwyag.py
@@ -145,3 +145,80 @@ def repo_find(path=".", required=True):
             return None
     
     return repo_find(parent, required)
+
+class GitObject(object):
+
+    def __init__(self, data=None) -> None:
+        if data:
+            self.deserialise(data)
+        else:
+            self.init()
+
+    def serialise(self, repo):
+        raise Exception("Unimplemented")
+    
+    def deserialise(self, data):
+        raise Exception("Unimplemented")
+    
+    def init(self):
+        pass
+
+def object_read(repo, sha):
+    """Read object sha from Git repository repo.  Return a
+    GitObject whose exact type depends on the object."""
+
+    path = repo_file(repo, "objects", sha[:2], sha[2:])
+
+    if not os.path.isfile(path):
+        return None
+    
+    with open(path, "rb") as f:
+        raw = zlib.decompress(f.read())
+
+        # read object type
+        x = raw.find(b' ')
+        fmt = raw[:x]
+
+        # read and validate object size
+        y = raw.find(b'\x00', x)
+        size = int(raw[x:y].decode('ascii'))
+
+        if size != len(raw) - y - 1:
+            raise Exception(f"Malformed objext {sha}: bad length")
+        
+        match fmt:
+            case b'commit'  : c=GitCommit
+            case b'tree'    : c=GitTree
+            case b'tag'     : c=GitTag
+            case b'blob'    : c=GitBlob
+            case _:
+                raise Exception(f"Unknown type {fmt.decode('ascii')} for object {sha}")
+            
+        return c(raw[y + 1:])
+
+def object_write(obj, repo=None):
+    data = obj.serialise()
+
+    # header
+    result = obj.fmt + b' ' + str(len(data)).encode() + b'\x00' + data
+
+    # compute hash
+    sha = hashlib.sha1(result).hexdigest()
+
+    if repo:
+        path = repo_file(repo, "objects", sha[:2], sha[2:], mkdir=True)
+
+        if not os.path.exists(path):
+            with open(path, 'wb') as f:
+                f.write(zlib.compress(result))
+
+    return sha
+
+class GitBlob(GitObject):
+    fmt = b'blob'
+
+    def serialise(self, repo):
+        return self.blobdata
+    
+    def deserialise(self, data):
+        self.blobdata = data
